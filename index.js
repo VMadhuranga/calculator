e574c931872840cefeae4936f611f3ccdbd2a722
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-07-01 15:26:53 +0530

    Create operate function
    This function call math operation functions based on operator

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// create variable for first number, second number and operator
+let firstNumber = 4;
+let secondNumber = 2;
+let operator = "/";
+
+console.log(operate(firstNumber, secondNumber, operator));
+
+// create function operate and call relevant function based on operator
+function operate(firstNumber, secondNumber, operator) {
+    if (operator === "+") {
+        return add(firstNumber, secondNumber);
+    } else if (operator === "-") {
+        return subtract(firstNumber, secondNumber);
+    } else if (operator === "*") {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator === "/") {
+        return divide(firstNumber, secondNumber);
+    }
+}
+
 // create functions for basic math operation
 // add
 function add(num1, num2) {
