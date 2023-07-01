37cd76a5cae05b429526d4772fee49b05c4fa0cd
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-07-01 15:20:50 +0530

    Create function for basic math operations
    Create add, subtract, multiply, divide functions

---
+++ index.js
@@ -0,0 +1,20 @@
+// create functions for basic math operation
+// add
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+// subtract
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+// multiply
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// divide
+function divide(num1, num2) {
+    return num1 / num2;
+}
