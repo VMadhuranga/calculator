47dbfaecfd8e17a758e14ae40651c79d8d39d91e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-07-03 09:53:51 +0530

    Rework index.js
    - Populate display when number button clicks
    - Add clear button
    - Add delete button
    - Add function to do math operation
    
    Additionally add CSS styling

--- index.js
+++ index.js
@@ -1,92 +1,82 @@
-// target display
 let display = document.querySelector(".display");
-
-// target number buttons
-let numbers = document.querySelectorAll(".btn.number");
-
-// target operator buttons
-let operators = document.querySelectorAll(".btn.operator");
-
-// target equal
-let equal = document.querySelector(".btn.equal");
-
-// target clear
-let clear = document.querySelector(".btn.clear");
-
-// add event listener to number buttons call function populateDisplay
-numbers.forEach((number) => {
-    number.addEventListener("click", populateDisplay);
-});
-
-// add event listener to operator buttons to call function selectOperator
-operators.forEach((operator) => {
-    operator.addEventListener("click", selectOperator);
-});
-
-// add event listener to equal button to call function equalize
-equal.addEventListener("click", equalize);
-
-// add event listener to clear button
-clear.addEventListener("click", clearDisplay);
+let buttons = [...document.querySelectorAll(".btn")];
 
 // create variable for first number, second number and operator
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
 
-// create function operate and call relevant function based on operator
+buttons.forEach((button) => {
+
+    // populate the display when number button clicks
+    if (button.classList[1] === "number") {
+        button.addEventListener("click", () => {
+            display.textContent += button.textContent;
+        });
+    }
+
+    // select operator and store first number
+    if (button.classList[1] === "operator") {
+        button.addEventListener("click", () => {
+            operator = button.textContent;
+            firstNumber = Number(display.textContent);
+            display.textContent = "";
+        });
+    }
+
+    // store second number and call function operate
+    if (button.classList[1] === "equation") {
+        button.addEventListener("click", () => {
+            secondNumber = Number(display.textContent);
+            display.textContent = operate(firstNumber, secondNumber, operator);
+        });
+    }
+
+    // clear all data
+    if (button.classList[1] === "clear") {
+        button.addEventListener("click", () => {
+            display.textContent = "";
+            firstNumber = 0;
+            secondNumber = 0;
+            operator = "";
+        });
+    }
+
+    // delete last item
+    if (button.classList[1] === "delete") {
+        button.addEventListener("click", () => {
+            display.textContent = display.textContent.slice(0, -1);
+        });
+    }
+
+});
+
+// create function operate and call relevant math operation functions based on operator
 function operate(firstNumber, secondNumber, operator) {
     if (operator === "+") {
         return add(firstNumber, secondNumber);
-    } else if (operator === "-") {
+    } if (operator === "-") {
         return subtract(firstNumber, secondNumber);
-    } else if (operator === "*") {
+    } if (operator === "*") {
         return multiply(firstNumber, secondNumber);
-    } else if (operator === "/") {
+    } if (operator === "/") {
         return divide(firstNumber, secondNumber);
     }
 }
 
 // create functions for basic math operation
-// add
-function add(num1, num2) {
-    return num1 + num2;
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
 }
 
-// subtract
-function subtract(num1, num2) {
-    return num1 - num2;
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
 }
 
-// multiply
-function multiply(num1, num2) {
-    return num1 * num2;
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
 }
 
-// divide
-function divide(num1, num2) {
-    return num1 / num2;
-}
-
-// create a function that populate the display when number button clicks
-function populateDisplay(event) {
-    display.textContent += event.target.textContent;
-}
-
-// create function to select operator and store first number
-function selectOperator(event) {
-    operator = event.target.textContent;
-    firstNumber = Number(display.textContent);
-    display.textContent = "";
-}
-
-// create function to store second number and call function operate
-function equalize() {
-    secondNumber = Number(display.textContent);
-    display.textContent = operate(firstNumber, secondNumber, operator);
-}
-
-// create function to clear display
-function clearDisplay() {
-    display.textContent = "";
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
 }
